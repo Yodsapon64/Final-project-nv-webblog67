2939aa3b31fbba5faf29e20b645bfaa777025fed
Poomeeii55 <poomeeii55@gmail.com>
2024-10-08 16:41:50 +0700

    edit User.js

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -1,38 +1,24 @@
-const Promise = require('bluebird')
-const bcrypt = Promise.promisifyAll(require('bcrypt-nodejs'))
-function hashPassword(user, options) {
-    const SALT_FACTOR = 8
-    if (!user.changed('password')) {
-        return
-    }
-    return bcrypt
-        .genSaltAsync(SALT_FACTOR)
-        .then(salt => bcrypt.hashAsync(user.password, salt, null))
-        .then(hash => {
-            user.setDataValue('password', hash)
-        })
-}
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/db');
 
-module.exports = (sequelize, DataTypes) => {
-    const User = sequelize.define('User', {
-        email: DataTypes.STRING,
-        password: DataTypes.STRING,
-        name: DataTypes.STRING,
-        lastname: DataTypes.STRING,
-        status: DataTypes.STRING,
-        type: DataTypes.STRING
-    }, {
-        hooks: {
-            beforeCreate: hashPassword,
-            beforeUpdate: hashPassword
-        }
-    })
+const User = sequelize.define('User', {
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  role: {
+    type: DataTypes.ENUM('user', 'admin'),
+    defaultValue: 'user',
+  },
+});
 
-    User.prototype.comparePassword = function (password) {
-        return bcrypt.compareSync(password, this.password)
-    }
-
-    User.associate = function (models) { }
-
-    return User
-}
+module.exports = User;
